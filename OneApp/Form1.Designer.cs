0c9f2b7d38f3b6672650aa9f1e116c76a9f31d8a
Renata Pedroso <renatanicollyy1@gmail.com>
2021-08-25 12:35:20 -0300

    First Commit

---
+++ OneApp/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿
+namespace OneApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtValor1 = new System.Windows.Forms.TextBox();
+            this.txtValor2 = new System.Windows.Forms.TextBox();
+            this.btnSomar = new System.Windows.Forms.Button();
+            this.resultado = new System.Windows.Forms.TextBox();
+            this.process1 = new System.Diagnostics.Process();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnSubtrair = new System.Windows.Forms.Button();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnDividir = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.calculadoraSimples = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.label1.Location = new System.Drawing.Point(127, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Valor 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.label2.Location = new System.Drawing.Point(127, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Valor 2:";
+            // 
+            // txtValor1
+            // 
+            this.txtValor1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtValor1.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtValor1.Location = new System.Drawing.Point(104, 83);
+            this.txtValor1.Name = "txtValor1";
+            this.txtValor1.Size = new System.Drawing.Size(100, 20);
+            this.txtValor1.TabIndex = 2;
+            this.txtValor1.TextChanged += new System.EventHandler(this.txtValor1_TextChanged);
+            // 
+            // txtValor2
+            // 
+            this.txtValor2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtValor2.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtValor2.Location = new System.Drawing.Point(104, 145);
+            this.txtValor2.Name = "txtValor2";
+            this.txtValor2.Size = new System.Drawing.Size(100, 20);
+            this.txtValor2.TabIndex = 3;
+            // 
+            // btnSomar
+            // 
+            this.btnSomar.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnSomar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSomar.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnSomar.ForeColor = System.Drawing.Color.Cornsilk;
+            this.btnSomar.Location = new System.Drawing.Point(47, 190);
+            this.btnSomar.Name = "btnSomar";
+            this.btnSomar.Size = new System.Drawing.Size(51, 41);
+            this.btnSomar.TabIndex = 4;
+            this.btnSomar.Text = "+";
+            this.btnSomar.UseVisualStyleBackColor = false;
+            this.btnSomar.Click += new System.EventHandler(this.btnSomar_Click);
+            // 
+            // resultado
+            // 
+            this.resultado.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.resultado.Enabled = false;
+            this.resultado.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.resultado.Location = new System.Drawing.Point(47, 258);
+            this.resultado.Name = "resultado";
+            this.resultado.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.resultado.Size = new System.Drawing.Size(222, 27);
+            this.resultado.TabIndex = 15;
+            this.resultado.TextChanged += new System.EventHandler(this.resultado_TextChanged);
+            // 
+            // process1
+            // 
+            this.process1.StartInfo.Domain = "";
+            this.process1.StartInfo.LoadUserProfile = false;
+            this.process1.StartInfo.Password = null;
+            this.process1.StartInfo.StandardErrorEncoding = null;
+            this.process1.StartInfo.StandardInputEncoding = null;
+            this.process1.StartInfo.StandardOutputEncoding = null;
+            this.process1.StartInfo.UserName = "";
+            this.process1.SynchronizingObject = this;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.Gold;
+            this.btnLimpar.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnLimpar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnLimpar.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnLimpar.ForeColor = System.Drawing.Color.Goldenrod;
+            this.btnLimpar.Location = new System.Drawing.Point(104, 307);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(100, 37);
+            this.btnLimpar.TabIndex = 16;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnSubtrair
+            // 
+            this.btnSubtrair.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnSubtrair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSubtrair.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnSubtrair.ForeColor = System.Drawing.Color.Cornsilk;
+            this.btnSubtrair.Location = new System.Drawing.Point(104, 190);
+            this.btnSubtrair.Name = "btnSubtrair";
+            this.btnSubtrair.Size = new System.Drawing.Size(51, 41);
+            this.btnSubtrair.TabIndex = 17;
+            this.btnSubtrair.Text = "-";
+            this.btnSubtrair.UseVisualStyleBackColor = false;
+            this.btnSubtrair.Click += new System.EventHandler(this.btnSubtrair_Click);
+            // 
+            // btnMultiplicar
+            // 
+            this.btnMultiplicar.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnMultiplicar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMultiplicar.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnMultiplicar.ForeColor = System.Drawing.Color.Cornsilk;
+            this.btnMultiplicar.Location = new System.Drawing.Point(218, 190);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(51, 41);
+            this.btnMultiplicar.TabIndex = 19;
+            this.btnMultiplicar.Text = "*";
+            this.btnMultiplicar.UseVisualStyleBackColor = false;
+            this.btnMultiplicar.Click += new System.EventHandler(this.btnMultiplicar_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(201, 363);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 15);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Renata  Pedroso";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // btnDividir
+            // 
+            this.btnDividir.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnDividir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDividir.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnDividir.ForeColor = System.Drawing.Color.Cornsilk;
+            this.btnDividir.Location = new System.Drawing.Point(161, 190);
+            this.btnDividir.Name = "btnDividir";
+            this.btnDividir.Size = new System.Drawing.Size(51, 41);
+            this.btnDividir.TabIndex = 21;
+            this.btnDividir.Text = "/";
+            this.btnDividir.UseVisualStyleBackColor = false;
+            this.btnDividir.Click += new System.EventHandler(this.btnDividir_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.Color.Gold;
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(305, 43);
+            this.listView1.TabIndex = 23;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // calculadoraSimples
+            // 
+            this.calculadoraSimples.AutoSize = true;
+            this.calculadoraSimples.BackColor = System.Drawing.Color.Gold;
+            this.calculadoraSimples.Font = new System.Drawing.Font("Showcard Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.calculadoraSimples.ForeColor = System.Drawing.Color.OliveDrab;
+            this.calculadoraSimples.Location = new System.Drawing.Point(48, 9);
+            this.calculadoraSimples.Name = "calculadoraSimples";
+            this.calculadoraSimples.Size = new System.Drawing.Size(222, 23);
+            this.calculadoraSimples.TabIndex = 24;
+            this.calculadoraSimples.Text = "Calculadora Simples";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(305, 387);
+            this.Controls.Add(this.calculadoraSimples);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnDividir);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnMultiplicar);
+            this.Controls.Add(this.btnSubtrair);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.resultado);
+            this.Controls.Add(this.btnSomar);
+            this.Controls.Add(this.txtValor2);
+            this.Controls.Add(this.txtValor1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtValor1;
+        private System.Windows.Forms.TextBox txtValor2;
+        private System.Windows.Forms.Button btnSomar;
+        private System.Windows.Forms.TextBox resultado;
+        private System.Diagnostics.Process process1;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnMultiplicar;
+        private System.Windows.Forms.Button btnSubtrair;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnDividir;
+        private System.Windows.Forms.Label calculadoraSimples;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ OneApp/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OneApp
+{
+    public partial class Form1 : Form
+    {
+        //declarando variáveis globalmente
+        float v1, v2, somar, subtrair, dividir, multiplicar;
+
+        //Inicializando o componente
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void resultado_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //bloco de Divisão
+        private void btnDividir_Click(object sender, EventArgs e)
+        {
+            float.TryParse(txtValor1.Text, out v1);
+            float.TryParse(txtValor2.Text, out v2);
+            dividir = v1 / v2;
+            resultado.Text = dividir.ToString();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //bloco de Subtração
+        private void btnSubtrair_Click(object sender, EventArgs e)
+        {
+            float.TryParse(txtValor1.Text, out v1);
+            float.TryParse(txtValor2.Text, out v2);
+            subtrair = v1 - v2;
+            resultado.Text = subtrair.ToString();
+        }
+
+        //bloco de Multiplicação
+        private void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            float.TryParse(txtValor1.Text, out v1);
+            float.TryParse(txtValor2.Text, out v2);
+            multiplicar = v1 * v2;
+            resultado.Text = multiplicar.ToString();
+        }
+
+        //bloco de Soma
+        private void btnSomar_Click(object sender, EventArgs e)
+        {
+
+            float.TryParse(txtValor1.Text, out v1);
+            float.TryParse(txtValor2.Text, out v2);
+            somar = v1 + v2;
+            resultado.Text = somar.ToString();
+
+        }
+
+        //Limpar
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtValor1.Clear();
+            txtValor2.Clear();
+            resultado.Clear();
+            txtValor1.Focus();
+        }
+
+        private void txtValor1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
